c8c8486904326a7787e0562739676a8d87af5382
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-21 04:05:20 -0700

    Task 5

---
+++ nop.c
@@ -0,0 +1,12 @@
+#include "monty.h"
+
+/**
+ * nop - does nothing
+ * @stack: Stack
+ * @line_number: Line Number
+ */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void) stack;
+	(void) line_number;
+}
