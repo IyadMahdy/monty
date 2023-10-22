d4b984ccd42914d69c60e118b732c1bcfec41926
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-22 08:11:47 -0700

    Fix Task 9

--- mod.c
+++ mod.c
@@ -9,6 +9,13 @@ void mod(stack_t **stack, unsigned int line_number)
 {
 	two_elements_check("mod", line_number);
 
+	if ((global.top)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		free_stack();
+		exit(EXIT_FAILURE);
+	}
+
 	global.top->prev->n %= global.top->n;
 	pop(stack, line_number);
 }
