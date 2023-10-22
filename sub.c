37e5df818a19911c9fa42fdef5217ebdf82f2ee4
Iyad Mahdy <iyad.mahdy@hotmail.com>
2023-10-22 08:04:03 -0700

    Stupid Fix Task 6

--- sub.c
+++ sub.c
@@ -7,11 +7,8 @@
  */
 void sub(stack_t **stack, unsigned int line_number)
 {
-	int res;
-
 	two_elements_check("sub", line_number);
 
-	res = (global.top)->n;
+	global.top->prev->n -= global.top->n;
 	pop(stack, line_number);
-	global.top->n = res - global.top->n;
 }
